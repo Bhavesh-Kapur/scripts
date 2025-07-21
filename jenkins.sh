f4d93269c256598ea0e0bb981eea5003cc2c4b47
Bhavesh Kapur <120770795+Bhavesh-Kapur@users.noreply.github.com>
2025-07-21 12:43:20 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -15,5 +15,4 @@ sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
-:wq
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
